7f772d7d19c45258d3a93d803bf630da0e2f7bff
Grant Rosario <grantster1124@aol.com>
2016-12-22 19:15:12 -0500

    Placed volcano markers in newly created feature group. Added layer control to toggle map features on and off.

--- script1.py
+++ script1.py
@@ -17,12 +17,17 @@ def color(elev):
         col = 'red'
     return col
 
+fg = folium.FeatureGroup(name="volcano locations")
+
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
-    map.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color=color(elev),icon_color='white')))
+    fg.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color=color(elev),icon_color='white')))
+
+map.add_child(fg)
 
 map.add_child(folium.GeoJson(data=open('World_pop.json'),
 name='World Population',
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red'}))
 
+map.add_child(folium.LayerControl())
 
 map.save(outfile='test.html')
