797a53909867f42f03f0fd776511e67232c66846
Grant Rosario <grantster1124@aol.com>
2016-12-22 19:08:51 -0500

    Added geojson file for international population levels and colorcoded map based on population

--- script1.py
+++ script1.py
@@ -3,7 +3,7 @@ import pandas
 
 df=pandas.read_csv("Volcanoes-USA.txt")
 
-map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()], zoom_start=6, tiles='Stamen Terrain')
+map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()], zoom_start=6, tiles='Mapbox Bright')
 
 def color(elev):
     minimum = int(min(df['ELEV']))
@@ -20,6 +20,9 @@ def color(elev):
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
     map.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color=color(elev),icon_color='white')))
 
+map.add_child(folium.GeoJson(data=open('World_pop.json'),
+name='World Population',
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red'}))
 
 
 map.save(outfile='test.html')
